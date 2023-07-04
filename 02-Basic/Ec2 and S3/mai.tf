b41e792062232c1ee985767c4d4c191e760deff0
Balapradeep <63641008+balapradeeps@users.noreply.github.com>
2023-07-04 16:03:24 +0530

    Update bucket and ami.tf

--- 02-Basic/Ec2 and S3/mai.tf
+++ 02-Basic/Ec2 and S3/mai.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "example" {
-  ami           = "ami-08e5424edfe926b43" # Ubuntu 20.04 LTS // us-east-1
+  ami           = "ami-08e5424edfe926c43" # Ubuntu 20.04 LTS // us-east-1
   instance_type = "t2.micro"
-  key_name = "FriendsKey"
+  key_name = "Friendskey"
   }
 
   resource "aws_s3_bucket" "terraform-state" {
-    bucket = "minbob-tf-st"
+    bucket = "minbob"
     versioning {
       enabled = true
     }
