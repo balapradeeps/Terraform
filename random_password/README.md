aacae64dbf9df2f9aafb3d96d41a7d5fcab85439
admin-fe <root@mail.com>
2023-07-10 19:35:07 +0530

    files modified

--- random_password/README.md
+++ random_password/README.md
@@ -2,6 +2,8 @@
 
 **Ignore the 8th line warning** 
 
+**8th number to numeric changed so warning won't come**
+
 ## To Get Password output use below command :
 
 ``terraform output -json``
--- random_password/random_password.tf
+++ random_password/random_password.tf
@@ -1,21 +1,15 @@
-resource "random_password" "autopassgen" {
+resource "random_password" "password-generator" {
   length           = var.length
   lower            = var.lower
   min_lower        = var.min_lower
   min_numeric      = var.min_numeric
   min_special      = var.min_special
   min_upper        = var.min_upper
-  number           = var.number
+  numeric          = var.number
   upper            = var.upper
   special          = var.special
   override_special = var.override_special
   keepers = {
     trigger = timestamp()
   }
-}
-
-output pass {
-  value     = random_password.autopassgen.result
-  description = "with keepers usage we generate random pass many times, each and every time while running we get pass with out destroy Beast note it "
-  sensitive = true
 }
