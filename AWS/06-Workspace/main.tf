01bc8bed78ab379523c8fc880fc1578472a9362b
Flex5 <Flex5@xamp.com>
2024-02-11 18:08:44 +0530

    undeclared resource fixed

--- AWS/06-Workspace/main.tf
+++ AWS/06-Workspace/main.tf
@@ -1,6 +1,6 @@
 module "ws" {
   source = "./modules/ec2"
   ami = var.ami
-  instance-type = lookup(var.instance_type, terraform.workspace, t2.micro)
+  instance-type = lookup(var.instance_type, terraform.workspace, "t2.micro")
   keypair = var.keypair
   }
