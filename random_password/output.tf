b401a3fac47bac5aaf27fe0693d63d9ec1861c9b
Balapradeep <63641008+balapradeeps@users.noreply.github.com>
2024-02-15 19:06:36 +0530

    Update output.tf

--- random_password/output.tf
+++ random_password/output.tf
@@ -1,5 +1,4 @@
 output password {
   value     = random_password.password-generator.result
-  description = "with keepers usage we generate random password many times,each and every time while running we get pass with out destroy Beast note it"
-  sensitive = true
+    sensitive = true
   }
