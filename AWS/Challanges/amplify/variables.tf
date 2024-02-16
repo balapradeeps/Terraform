8da123b63cfd38e29a6de0f620d2a11b3482623f
Balapradeep <63641008+balapradeeps@users.noreply.github.com>
2024-02-17 00:10:58 +0530

    Random_update of amplify

---
+++ AWS/Challanges/amplify/variables.tf
@@ -0,0 +1,8 @@
+variable bitbucket_frontend_repository {
+  type = string
+}
+
+variable enable_amplify_frontend_create {
+  type        = bool
+  description = "To enable the amplify frontend app creation[ possible values : true, null]"
+}
