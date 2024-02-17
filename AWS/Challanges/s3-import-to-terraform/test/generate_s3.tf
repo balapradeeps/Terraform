752175e5ed89e621ed1aafe60c350e90cc2b66a0
Balapradeep <63641008+balapradeeps@users.noreply.github.com>
2024-02-17 21:53:58 +0530

    Import-test-scenario-01

---
+++ AWS/Challanges/s3-import-to-terraform/test/generate_s3.tf
@@ -0,0 +1,14 @@
+# __generated__ by Terraform
+# Please review these resources and move them into your main configuration files.
+
+# __generated__ by Terraform from "ws-bckusp"
+/*
+resource "aws_s3_bucket" "mystorage" {
+  bucket              = "ws-bckusp"
+  bucket_prefix       = null
+  force_destroy       = null
+  object_lock_enabled = false
+  tags                = {}
+  tags_all            = {}
+}
+*/
---
+++ AWS/Challanges/s3-import-to-terraform/test/import-old-main.tf
@@ -0,0 +1,9 @@
+/* provider "aws" {
+  region = "us-east-1"
+}
+
+import {
+      id = "i-05e5fc08918d07101" 
+
+    to = aws_instance.myinstance
+} */
