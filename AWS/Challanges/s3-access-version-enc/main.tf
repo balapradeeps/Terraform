dea763f635024da8d6e27cb9c4fd45dfea9748a9
Balapradeep <63641008+balapradeeps@users.noreply.github.com>
2024-02-25 19:27:02 +0530

    For module update

--- AWS/Challanges/s3-access-version-enc/main.tf
+++ AWS/Challanges/s3-access-version-enc/main.tf
@@ -1,46 +1,8 @@
-resource "aws_s3_bucket" "mybucket" {
-    bucket = var.bucketname
-    
+module "s3-crs-org" {
+  source = "./modules/s3-cross-orgin"
+  bucketname = "losags-kodai-bake-901"
+
     tags = {
-      Terraform = "true"
-      Enviroment = "dev"
+      Enviroment = "Dev"
     }
- }
-
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.mybucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
- resource "aws_s3_bucket_acl" "example" {
-  depends_on = [aws_s3_bucket_ownership_controls.example]
-
-  bucket = aws_s3_bucket.mybucket.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.mybucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_cors_configuration" "example" {
-  bucket = aws_s3_bucket.mybucket.id
-
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["PUT", "POST"]
-    allowed_origins = ["https://s3-website-test.hashicorp.com"]
-    expose_headers  = ["ETag"]
-    max_age_seconds = 3000
-  }
-
-  cors_rule {
-    allowed_methods = ["GET"]
-    allowed_origins = ["*"]
-  }
 }
