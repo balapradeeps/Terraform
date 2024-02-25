ba892c483747b5cc1228c29f8bb0dd4c12698d59
Balapradeep <63641008+balapradeeps@users.noreply.github.com>
2024-02-25 18:22:14 +0530

    cross-config

--- AWS/Challanges/s3-access-version-enc/main.tf
+++ AWS/Challanges/s3-access-version-enc/main.tf
@@ -27,3 +27,20 @@ resource "aws_s3_bucket_versioning" "versioning" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_cors_configuration" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["https://s3-website-test.hashicorp.com"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+
+  cors_rule {
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+  }
+}
