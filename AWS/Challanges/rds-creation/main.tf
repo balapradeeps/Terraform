6b14f557f9d47d4a0df04d2b7769a44dac47237b
Balapradeep <63641008+balapradeeps@users.noreply.github.com>
2024-03-05 08:31:05 +0530

    Basic-Creatation&ec2connect-Working

--- AWS/Challanges/rds-creation/main.tf
+++ AWS/Challanges/rds-creation/main.tf
@@ -1,3 +1,17 @@
+resource "random_password" "password_generator" {
+  length       = 12
+  lower        = true
+  min_lower    = 3   
+  min_numeric  = 3   
+  min_special  = 3   
+  min_upper    = 3   
+  numeric      = true
+  special      = true
+  upper        = true
+  
+  }
+
+
 resource "aws_db_instance" "taskdb" {
     identifier           = "my-db-instance"  # Provide your desired DB identifier here
     allocated_storage    = 10
@@ -6,7 +20,29 @@ resource "aws_db_instance" "taskdb" {
     engine_version       = "5.7"
     instance_class       = "db.t3.micro"
     username             = "terrauser"
-    password             = "MaeU8!L#0BX"
+    password             = random_password.password_generator.result
     parameter_group_name = "default.mysql5.7"
     skip_final_snapshot  = true
+}
+
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "Monkey-D"
+  public_key = file("~/.ssh/id_rsa.pub")  # Update with the path to your public key
+}
+
+
+resource "aws_instance" "rdsconnecter" {
+    ami = "ami-0cd59ecaf368e5ccf"
+    instance_type = "t2.micro"
+    key_name      = aws_key_pair.my_key_pair.key_name  
+}
+
+
+output "rdsconnecterip" {
+  value = aws_instance.rdsconnecter.public_ip
+}
+
+output "password_result" {
+  value = random_password.password_generator.result
+  sensitive = true
 }
