8311f1f55460bed64ba9f68ad5db01b7ba8c2ba5
Balapradeep <63641008+balapradeeps@users.noreply.github.com>
2024-10-08 14:06:58 +0530

    Update main.tf with notes correction

--- AWS/Challanges/AWS_ECR/main.tf
+++ AWS/Challanges/AWS_ECR/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "demo2" {
   name = "demo2"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
