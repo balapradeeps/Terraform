295da178f4ffa85159a46c0167d1d9b4ea8409ad
Balapradeep <63641008+balapradeeps@users.noreply.github.com>
2024-10-08 14:06:18 +0530

    Update docker-build-push.tf with notes correction

--- AWS/Challanges/AWS_ECR/docker-build-push.tf
+++ AWS/Challanges/AWS_ECR/docker-build-push.tf
@@ -7,7 +7,7 @@ resource "null_resource" "docker_build_and_push"{
     aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin ${data.aws_ecr_authorization_token.auth.proxy_endpoint}
 
   # Build the Docker Image 
-    docker build -t demo2todo-app ./
+    docker build -t demo2todo-app .
   # Tag the Docker Image 
     docker tag demo2todo-app:latest ${aws_ecr_repository.demo2.repository_url}:latest
   # Push the Docker Image to ECR
